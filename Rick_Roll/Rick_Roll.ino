eaff5b7c73b0d788a9708632442b700fb72e37c2
Big Curt <curtis.fields@muskegoncc.edu>
2018-10-15 22:07:17 -0400

    updated rick roll

--- Rick_Roll/Rick_Roll.ino
+++ Rick_Roll/Rick_Roll.ino
@@ -55,9 +55,19 @@ void setup() {
 }
 
 void loop() {
+  
   String str = "We're no strangers to love You know the rules and so do I A full commitment's what I'm thinking of You wouldn't get this from any other guy I just wanna tell you how I'm feeling Gotta make you understand Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you We've known each other for so long Your heart's been aching but you're too shy to say it Inside we both know what's been going on We know the game and we're gonna play it And if you ask me how I'm feeling Don't tell me you're too blind to see Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give, never gonna give (Give you up) (Ooh) Never gonna give, never gonna give (Give you up) We've known each other for so long Your heart's been aching but you're too shy to say it Inside we both know what's been going on We know the game and we're gonna play it I just wanna tell you how I'm feeling Gotta make you understand Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry";
+  
+  //create strings for top(A) and bottom(B) halves of display
+  //pre-load first part of string into B
   String subA;
   String subB = str.substring(0, 16);
+
+  //grab letters in chucks of 16
+  //set b text to a
+  //check for last " "
+  //chop string
+  //set last position to begining of next word
   for(int pos = 16; pos <= str.length(); pos += 16){
     lcd.clear();
     lcd.setCursor(0,0);
@@ -65,11 +75,13 @@ void loop() {
     subB = str.substring(pos, pos+16);
     int p = subB.lastIndexOf(" ", subB.length());
     subB = subB.substring(0, p);
-    pos = pos - (16 - p) + 1;
+    if(p!=-1){
+     pos = pos - (16 - p) + 1;
+    }
     lcd.print(subA);
     lcd.setCursor(0,1);
     lcd.print(subB);
-    delay(3000);
+    delay(1000);
   }
   
 }
